bc266ff3621d4a106eac06882a7ac86834e061df
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-10-12 19:59:24 -0300

    inicializando aplicação apenas com recurso index.html

---
+++ src/main/java/com/springbootproject/runner/RunnerApplication.java
@@ -0,0 +1,15 @@
+package com.springbootproject.runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnerApplication.class, args);
+
+    System.out.println("oi");
+	}
+
+}
